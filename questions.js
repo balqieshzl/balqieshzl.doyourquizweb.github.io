69aeedd6f438a4e60dbf8260c495635f53aedc5a
balqieshzl <149229719+balqieshzl@users.noreply.github.com>
2024-07-09 10:03:37 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Which of the following has the least value?",
+        answer:"C. (4 − 3) − (5 × 0)",
+        options: [
+            "A. (4 × 3) × (5 − 0)",
+            "B. (4 × 3) − (5 × 0)", 
+            "C. (4 − 3) − (5 × 0)", 
+            "D. (4 − 3) × (5 − 0)",
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which of the following has the greatest value?",
+        answer:"D. (5 × 4) × (7 − 0)",
+        options: [
+            "A. (5 × 4) − (7 × 0)", 
+            "B. (5 − 4) − (7 × 0)", 
+            "C. (5 − 4) × (7 − 0)", 
+            "D. (5 × 4) × (7 − 0)",
+        ]
+    },
+    {
+        numb: 3,
+        question: "You have a 10-digit number. When you add all the digits the sum is 9. What do you get when you multiply all the digits?",
+        answer:"D. 0",
+        options: [
+            "A. 1",
+            "B. 45", 
+            "C. 9 x 8 x 7 x ... x 2 x 1",
+            "D. 0",
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is the value of (5 × 0) + (5 × 4)?",
+        answer:"B. 20",
+        options: [
+            "A. 25", 
+            "B. 20", 
+            "C. 5", 
+            "D. 0",
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which of these is Seven hundred sixty?",
+        answer:"D. 760",
+        options: [
+            "A. 670", 
+            "B. 706", 
+            "C. 716", 
+            "D. 760",
+        ]
+    },
+]
